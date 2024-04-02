45611db7d366f64a42fe866c667f7fe96106a2e0
MattiaMessana <mattia.messana@icloud.com>
2024-04-02 15:53:03 +0200

    add:snak3

---
+++ jsSnak3/js/script.js
@@ -0,0 +1,43 @@
+//TEST
+// alert('ciao');
+
+// Il software deve chiedere per 10 volte all'utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+//1. chiediamo tramite pormpt all'tuente di inserire 10 numeri 
+const num1 = Number(prompt('dimmi un numero'));
+console.log(num1);
+
+const num2 = Number(prompt('dimmi un numero'));
+console.log(num2);
+
+const num3 = Number(prompt('dimmi un numero'));
+console.log(num3);
+
+const num4 = Number(prompt('dimmi un numero'));
+console.log(num4);
+
+const num5 = Number(prompt('dimmi un numero'));
+console.log(num5);
+
+const num6 = Number(prompt('dimmi un numero'));
+console.log(num6);
+
+const num7 = Number(prompt('dimmi un numero'));
+console.log(num7);
+
+const num8 = Number(prompt('dimmi un numero'));
+console.log(num8);
+
+const num9 = Number(prompt('dimmi un numero'));
+console.log(num9);
+
+const num10 = Number(prompt('dimmi un numero'));
+console.log(num10);
+
+
+//2. sommiamo numeri inseirti 
+
+const sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10
+console.log(sum);
+
