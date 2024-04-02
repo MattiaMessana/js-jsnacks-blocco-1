bc6e95496530cc59d94c099e768af7add1081ffb
MattiaMessana <mattia.messana@icloud.com>
2024-04-02 17:58:32 +0200

    add:Snak7

---
+++ jsSnak7/js/script.js
@@ -0,0 +1,9 @@
+//TEST
+// alert('ciao');
+
+//Stampa le potenze di 2 fino a 1000.
+
+//1. creiamo un ciclo for che calcoli le potenze di due fino a 1000
+for (let i = 2; i <= 1000 ; i *= 2 ) {
+    console.log(i);
+};
